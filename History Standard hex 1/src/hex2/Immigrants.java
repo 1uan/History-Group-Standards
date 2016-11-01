a891ba9cfd7e2f915bc9394f115351160f955159
Juan Halleran <computer@10.153.112.118>
2016-11-01 08:41:20 -0400

    fixed merge issue

--- History Standard hex 1/src/hex2/Immigrants.java
+++ History Standard hex 1/src/hex2/Immigrants.java
@@ -11,41 +11,41 @@ public class Immigrants {
 					+ "\n [2] Learn about the Chinese Exclusion Act"
 					+ "\n [3] Learn about the Gentleman's Agreement with Japan"
 					+ "\n [0] Exit Program\n");
-		}
-		String input = scanner.nextLine();
-		int inputNumb = Integer.parseInt(input);
-
-		switch (inputNumb) {
-		case 1:
-			//Overall
-			System.out.print("Compare the experience of European immigrants in the east to that of Asian immigrants in the west\n"
-					+ "Immigration to the west coast mainly consisted of Chinese immigrants. Chinese immigrants worked on the railroad "
-					+ "\nand moved somewhat throughout the west. Immigration in the east mainly consisted of southern and eastern European "
-					+ "\nimmigrants. They would arrive at Ellis Island and rarely move about the east.\n\n");
-			break;
-		case 2:
-			//Chinese Exclusion Act
-			System.out.println("The Chinese Exclusion Act was a law passed by Congress in 1882 that denied U.S. citizenship to people"
-					+ "\n born in China and banned immigration from China for 10 years. This act marked the only time in U.S. history"
-					+ "\n that a specific nationality was banned from immigrating to the country. For many years, the Chinese"
-					+ "\n Exclusion Act limited the Chinese population in the United States. The act also shaped native citizens'"
-					+ "\n attitudes about the Chinese.");
-
-			break;
-		case 3:
-			//Gentleman's Agreement with Japan
-			System.out.print("The Gentlemen's Agreement was an agreement between the U.S. government and Japan that limited the "
-					+ "\n number of Japanese unskilled workers immigrating to the United States in 1907 and prompted the United States"
-					+ "\n to end anti-Japanese laws and segregation. For many years, the Gentlemen's Agreement limited the Japanese population"
-					+ "\n in the United States.");
-			break;
-		case 0:
-			continueVar = false;
-			System.out.println("Exiting Immigrants");
-			break;
-		default: 
-			break;
-
+		
+			String input = scanner.nextLine();
+			int inputNumb = Integer.parseInt(input);
+	
+			switch (inputNumb) {
+			case 1:
+				//Overall
+				System.out.print("Compare the experience of European immigrants in the east to that of Asian immigrants in the west\n"
+						+ "Immigration to the west coast mainly consisted of Chinese immigrants. Chinese immigrants worked on the railroad "
+						+ "\nand moved somewhat throughout the west. Immigration in the east mainly consisted of southern and eastern European "
+						+ "\nimmigrants. They would arrive at Ellis Island and rarely move about the east.\n\n");
+				break;
+			case 2:
+				//Chinese Exclusion Act
+				System.out.println("The Chinese Exclusion Act was a law passed by Congress in 1882 that denied U.S. citizenship to people"
+						+ "\n born in China and banned immigration from China for 10 years. This act marked the only time in U.S. history"
+						+ "\n that a specific nationality was banned from immigrating to the country. For many years, the Chinese"
+						+ "\n Exclusion Act limited the Chinese population in the United States. The act also shaped native citizens'"
+						+ "\n attitudes about the Chinese.");
+	
+				break;
+			case 3:
+				//Gentleman's Agreement with Japan
+				System.out.print("The Gentlemen's Agreement was an agreement between the U.S. government and Japan that limited the "
+						+ "\n number of Japanese unskilled workers immigrating to the United States in 1907 and prompted the United States"
+						+ "\n to end anti-Japanese laws and segregation. For many years, the Gentlemen's Agreement limited the Japanese population"
+						+ "\n in the United States.");
+				break;
+			case 0:
+				continueVar = false;
+				System.out.println("Exiting Immigrants");
+				break;
+			default: 
+				break;
+			}
 		}
 	}
 }
